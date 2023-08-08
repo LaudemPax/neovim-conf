aca5c6a6683ca0d33548fca7a54e9ed2cec9714c
LaudemPax <aman.tifli@gmail.com>
2023-08-08 16:06:39 +0200

    fixed LSP

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -7,8 +7,6 @@ lsp.ensure_installed({
   "eslint",
   -- markdown
   "marksman",
-  -- nix
-  "nil_ls",
   -- python
   "pyre",
 })
@@ -18,6 +16,18 @@ lsp.on_attach(function(client, bufnr)
   -- to learn the available actions
   lsp.default_keymaps({buffer = bufnr})
 end)
+
+
+-- key remap
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  mapping = {
+    ['<Tab>'] = cmp_action.tab_complete(),
+    ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
+  }
+})
 
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
