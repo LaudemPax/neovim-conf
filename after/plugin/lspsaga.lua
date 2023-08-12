585c2f8b52b9ec989d10b4bb7bb84a32c65033ba
LaudemPax <aman.tifli@gmail.com>
2023-08-12 13:46:31 +0200

    trying new keybindings

--- after/plugin/lspsaga.lua
+++ after/plugin/lspsaga.lua
@@ -2,4 +2,4 @@ local saga = require('lspsaga')
 
 saga.setup({})
 vim.keymap.set("n", "<C-r>", "<cmd>Lspsaga rename<cr>")
-vim.keymap.set("n", "<C-t>", "<cmd>Lspsaga term_toggle<cr>")
+vim.keymap.set("n", "T", "<cmd>Lspsaga term_toggle<cr>")
