b43f299405074a9af8f4e9fe2fc7c4cad26d4dd3
LaudemPax <aman.tifli@gmail.com>
2023-08-19 00:02:45 +0200

    added leap.nvim

--- lua/devel/packer.lua
+++ lua/devel/packer.lua
@@ -33,6 +33,7 @@ return require('packer').startup(function(use)
   use("nvim-treesitter/nvim-treesitter-context");
   use("nvim-tree/nvim-tree.lua")
   use("akinsho/toggleterm.nvim")
+  use("ggandor/leap.nvim")
 
   -- autpairs
   use {
