8d5f65b71e4c8db9e002f158a1a82c68e1937d81
MuhammadAman BinAhmadTifli <MuhammadAman.BinAhmadTifli@kuka.com>
2023-08-22 09:39:24 +0200

    added copy whole file to clipboard

--- lua/devel/remap.lua
+++ lua/devel/remap.lua
@@ -1,3 +1,10 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Define a custom command that copies the entire buffer to the clipboard
+vim.cmd([[
+  command! CopyWholeFileToClipboard :%yank + | echom 'Entire file copied to clipboard'
+]])
+-- Map a key to trigger the custom command
+vim.api.nvim_set_keymap('n', '<leader>ca', ':CopyWholeFileToClipboard<CR>', { noremap = true, silent = true })
+
