797c981a5d15e2cbfb121a4a5a7e13ddd63e9951
MuhammadAman BinAhmadTifli <MuhammadAman.BinAhmadTifli@kuka.com>
2023-08-29 12:43:42 +0200

    lsp saga keymaps

--- after/plugin/lspsaga.lua
+++ after/plugin/lspsaga.lua
@@ -1,4 +1,9 @@
 local saga = require('lspsaga')
 
 saga.setup({})
+
+-- rename
 vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<cr>")
+
+-- find usages
+vim.keymap.set("n", "<leader>fu", "<cmd>Lspsaga finder<cr>")
