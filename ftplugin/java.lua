e321d05fb1b5ca51fef7f8bb1be71d197df6d215
LaudemPax <aman.tifli@gmail.com>
2023-12-26 18:19:12 +0100

    changed java tab size to 8

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -3,3 +3,8 @@ local config = {
     root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
 }
 require('jdtls').start_or_attach(config)
+
+local o = vim.opt_local
+o.expandtab = true
+o.softtabstop = 8
+o.shiftwidth = 8
